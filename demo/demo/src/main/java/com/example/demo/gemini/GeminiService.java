13725c4476cf05c1a8b06cd862f94c0ca519f5ef
RafaelBocsa <mrraf995@gmail.com>
2025-11-26 18:24:46 -0500

    gemini connection leaks fix, connection pooler settings

--- demo/demo/src/main/java/com/example/demo/gemini/GeminiService.java
+++ demo/demo/src/main/java/com/example/demo/gemini/GeminiService.java
@@ -2,6 +2,7 @@ package com.example.demo.gemini;
 
 import com.example.demo.user.User;
 import com.example.demo.user.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,34 +28,41 @@ public class GeminiService {
         this.userRepository = userRepository;
     }
 
+
     public String askGemini(String uuid, String question) {
-        User user = userRepository.findById(uuid).orElseThrow(() -> new IllegalStateException("User with id " + uuid+ " doesn't exist"));
+        decrementGeminiRequests(uuid);
+        return callGemini(question);
+    }
+
+    @Transactional
+    public void decrementGeminiRequests(String uuid) {
+        User user = userRepository.findById(uuid)
+                .orElseThrow(() -> new IllegalStateException("User not found"));
+
         int req = user.getRequests();
-        String response;
-        if(req > 0){
-            int newReq = req-1;
-            user.setRequests(newReq);
-            userRepository.save(user);
-            Map<String, Object> requestBody = Map.of(
-                    "contents", new Object[]{
-                            Map.of("parts", new Object[]{
-                                    Map.of("text", question)
-                            })
-                    }
-            );
-
-
-            response = webClient.post()
-                    .uri(geminiApiUrl  + geminiApiKey )
-                    .header("Content-Type" , "application/json")
-                    .bodyValue(requestBody)
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
-        }else{
-            response = "Hit daily Gemini requests";
+        if (req <= 0) {
+            throw new IllegalStateException("No remaining requests.");
         }
 
-        return response;
+        user.setRequests(req - 1);
+        userRepository.save(user);
+    }
+
+    private String callGemini(String question) {
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", question)
+                        })
+                }
+        );
+
+        return webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 }
